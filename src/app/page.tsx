5b3f0822632005911ff1b62356b8ffa5fd19274c
EphriamHab <ephremhabtamu2015@gmail.com>
2024-07-31 19:23:51 +0300

    page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,15 +3,26 @@ import Home from "@/components/home";
 import About from "@/components/about";
 import Services from "@/components/services";
 import Skills from "@/components/skills";
+import Projects from "@/components/projects";
+import Contact from "@/components/contact";
+import Footer from "@/components/footer";
+import Certification from "@/components/certification";
+
 
 export default function Page() {
   return (
-    <div className="gird gap-8">
+    <>
       <Header />
-      <Home/>
-      <About/>
-      <Services/>
-      <Skills/>
-    </div>
+      <main>
+        <Home />
+        <About />
+        <Services />
+        <Skills />
+        <Projects />
+        <Certification/>
+        <Contact />
+      </main>
+      <Footer />
+    </>
   );
 }

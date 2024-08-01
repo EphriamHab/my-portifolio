4d83f728f1d3e74b4e13c008c4bae385f2b53834
EphriamHab <ephremhabtamu2015@gmail.com>
2024-08-01 15:12:44 +0300

    footer

---
+++ src/components/footer.tsx
@@ -0,0 +1,10 @@
+export default function Footer() {
+    return (
+      <footer className="w-full p-4 mt-4 shadow-xl">
+        <p className="text-center font-serif font-semibold text-sm md:text-base">
+          &copy; 2024 Made By Ephrem
+        </p>
+      </footer>
+    );
+  }
+  

891ae1adb63de65e8ae1e5b4a49e3781c8ac6ed0
EphriamHab <ephremhabtamu2015@gmail.com>
2024-11-25 11:02:09 +0300

    project

--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -15,9 +15,7 @@ export default function Projects() {
             <img
               src="H1.png"
               alt="Ecommerce WebApp"
-              width={600}
-              height={300}
-              className="w-full rounded-lg transition-transform duration-500 group-hover:scale-105"
+              className="w-full h-[250px] rounded-lg transition-transform duration-500 group-hover:scale-105"
             />
             <div className="absolute inset-0 bg-gradient-to-t from-blue-500 to-transparent opacity-0 group-hover:opacity-100 transition-opacity duration-500 flex flex-col items-center justify-center text-center p-10">
               <h1 className="text-gray-950 font-bold text-lg mb-4">
@@ -42,9 +40,7 @@ export default function Projects() {
             <img
               src="p1.png"
               alt="Blog App"
-              width={600}
-              height={400}
-              className="w-full rounded-lg transition-transform duration-500 group-hover:scale-105"
+              className="w-full h-[250px] rounded-lg transition-transform duration-500 group-hover:scale-105"
             />
             <div className="absolute inset-0 bg-gradient-to-t from-blue-500 to-transparent opacity-0 group-hover:opacity-100 transition-opacity duration-500 flex flex-col items-center justify-center text-center p-10">
               <h1 className="text-gray-950 font-bold text-lg mb-4">Blog App</h1>
@@ -67,12 +63,12 @@ export default function Projects() {
             <img
               src="expense.png"
               alt="Expense Tracker"
-              width={600}
-              height={300}
-              className="w-full rounded-lg transition-transform duration-500 group-hover:scale-105"
+              className="w-full h-[250px] rounded-lg transition-transform duration-500 group-hover:scale-105"
             />
             <div className="absolute inset-0 bg-gradient-to-t from-blue-500 to-transparent opacity-0 group-hover:opacity-100 transition-opacity duration-500 flex flex-col items-center justify-center text-center p-10">
-              <h1 className="text-gray-950 font-bold text-lg mb-4">Expense Tracker</h1>
+              <h1 className="text-gray-950 font-bold text-lg mb-4">
+                Expense Tracker
+              </h1>
               <p className="text-sm leading-7 text-gray-950 font-semibold">
                 Track expenses effortlessly with our React-based app. Manage
                 spending, stay on budget, and take control of your finances with
@@ -92,16 +88,41 @@ export default function Projects() {
             <img
               src="yeneta.PNG"
               alt="yeneta tutor"
-              width={600}
-              height={300}
-              className="w-full rounded-lg transition-transform duration-500 group-hover:scale-105"
+              className="w-full h-[250px] rounded-lg transition-transform duration-500 group-hover:scale-105"
             />
             <div className="absolute inset-0 bg-gradient-to-t from-blue-500 to-transparent opacity-0 group-hover:opacity-100 transition-opacity duration-500 flex flex-col items-center justify-center text-center p-10">
-              <h1 className="font-bold text-gray-950 text-lg mb-4">የኔታ Tutor</h1>
+              <h1 className="font-bold text-gray-950 text-lg mb-4">
+                የኔታ Tutor
+              </h1>
               <p className="text-sm leading-7 text-gray-950 font-bold">
                 የኔታ Tutor is a Flutter-based mobile app connecting students with
                 skilled tutors for quality, affordable at-home learning,
                 designed to support student success.
+              </p>
+              <Link
+                href="https://github.com/Yeneta-tutors/yeneta_tutor.git"
+                legacyBehavior
+              >
+                <a className="text-custom-purple mt-4 text-4xl w-32 h-32 flex items-center justify-center rounded-full">
+                  <i className="bx bxl-github"></i>
+                </a>
+              </Link>
+            </div>
+          </div>
+          <div className="relative overflow-hidden rounded-lg cursor-pointer group">
+            <img
+              src="C.JPG"
+              alt="Ethio Chat"
+              className="w-full h-[250px] rounded-lg transition-transform duration-500 group-hover:scale-105"
+            />
+            <div className="absolute inset-0 bg-gradient-to-t from-blue-500 to-transparent opacity-0 group-hover:opacity-100 transition-opacity duration-500 flex flex-col items-center justify-center text-center p-10">
+              <h1 className="font-bold text-gray-950 text-lg mb-4">
+                EthioChat
+              </h1>
+              <p className="text-sm leading-7 text-gray-950 font-bold">
+                EthioChat is a Flutter-based mobile app that enables seamless,
+                secure, and real-time messaging, helping users stay connected
+                with friends and family anytime, anywhere.
               </p>
               <Link
                 href="https://github.com/Yeneta-tutors/yeneta_tutor.git"
